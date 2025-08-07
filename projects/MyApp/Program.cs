cef617a444d323ab0074f901a9738e5fd25a187f
Mohammed Saeed <mohammedsaead643@gmail.com>
2025-08-07 19:43:59 +0300

    initial commit

---
+++ projects/MyApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO.Pipelines;
+namespace TourOfCsharp;
+
+class Employee
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
