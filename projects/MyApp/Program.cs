cb4e203b9bd752bbd3ff1f58512d4fbfcdf03d3a
Mohammed Saeed <mohammedsaead643@gmail.com>
2025-08-07 20:51:10 +0300

    edits

--- projects/MyApp/Program.cs
+++ projects/MyApp/Program.cs
@@ -3,11 +3,28 @@ using System.Diagnostics;
 using System.IO.Pipelines;
 namespace TourOfCsharp;
 
-class Employee
+using System;
+
+public class GalacticTravelAgency
 {
-    static void Main()
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Your code goes here
+        string passengerName = "Zara";
+        int passengerAge = 28;
+        string ticketType = "First Class";
+        string preferredPlanet = "Mars";
+        Console.WriteLine(passengerName);
+        Console.WriteLine(passengerAge);
+        Console.WriteLine(ticketType);
+        Console.WriteLine(preferredPlanet);
+        passengerAge++;
+        Console.WriteLine(passengerAge);
+        double passengerAgeDouble = (double)passengerAge;
+        Console.WriteLine(passengerAgeDouble);
+        double passengerAgeDouble2 = passengerAge;
+        Console.WriteLine(passengerAgeDouble2);
+        string passengerAgeString = Convert.ToString(passengerAge);
+        Console.WriteLine(passengerAgeString);
     }
 }
-
